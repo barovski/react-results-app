d4f878a9f13fad3475ead5d80a2306a9ceca6d30
bobbykolev <bobbykolev@abv.bg>
2015-05-15 12:02:24 +0300

    footer events (page up/page down) was added

--- app/js/components/footer.js
+++ app/js/components/footer.js
@@ -1,12 +1,39 @@
 var React = require('react');
 
 module.exports = React.createClass({
-  displayName : 'footer',
-  render: function () {
-    return (
-    	<footer className="main-footer">
-      		<i className="fa fa-bars fa-3"></i>
-    	</footer>
-    );
-  }
+        displayName: 'footer',
+        _setFooterMenuEvents: function() {
+            var main = $('main'),
+                container = $('.main-container'),
+                footDown = $(this.refs.footDown.getDOMNode()),
+                footUp = $(this.refs.footUp.getDOMNode());
+
+            footDown.on('click', function(e) {
+                main.animate({
+                    scrollTop: container.height()
+                }, 'slow');
+            });
+
+            footUp.on('click', function(e) {
+                main.animate({
+                    scrollTop: 0
+                }, 'slow');
+            });
+        },
+        componentDidMount: function() {
+            this._setFooterMenuEvents();
+        },
+        render: function() {
+		    return (
+				<footer className="main-footer">
+					<div className="left">
+						<i ref="footMenu" className="fa fa-bars fa-3"></i>
+					</div>
+					<div className="right">
+						<i ref="footDown" className="fa fa-level-down fa-3"></i>
+						<i ref="footUp" className="fa fa-level-up fa-3"></i>
+					</div>
+				</footer>
+			);
+  		}
 });
